830489ef5e9bd69454ae83674211e41da18c90e7
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-15 19:30:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,17 +20,8 @@ class Dog
     @dogs_breed = "Beagle"
   end
 end
-fido = Dog.new
-fido.name ="Fido"
-fido.breed == "Beagle"
-fido.name
-fido.breed
-class Dog
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
-
-  def name
-    @this_dogs_name
-  end
-end
+# fido = Dog.new
+# fido.name = "Fido"
+# fido.breed = "Beagle"
+# fido.name
+# fido.breed
